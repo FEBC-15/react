b6b54387d415cbb212b78678755417f96eefc8e5
kilyong jeong <uzoolove@gmail.com>
2025-12-23 15:38:04 +0900

    Store 생성

--- workspace-ins/ch07-globalstate/01-redux/src/redux/store.ts
+++ workspace-ins/ch07-globalstate/01-redux/src/redux/store.ts
@@ -1,2 +1,9 @@
 // TODO 3. Store 생성
 
+// Redux에서는 redux-toolkit 사용을 권장
+import { createStore } from "redux";
+import counterReducer from "@/redux/counterReducer";
+
+const store = createStore(counterReducer);
+
+export default store;
