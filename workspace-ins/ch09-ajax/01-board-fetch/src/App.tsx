b6c9514f9ea32ebe1bd04c81492c9452ccf5a38f
kilyong jeong <uzoolove@gmail.com>
2025-12-30 09:11:39 +0900

    TODO 1: public/boardinfo.html 파일과 동일한 화면이 보이도록 리액트 컴포넌트 만들기

--- workspace-ins/ch09-ajax/01-board-fetch/src/App.tsx
+++ workspace-ins/ch09-ajax/01-board-fetch/src/App.tsx
@@ -2,9 +2,11 @@
 //  http://localhost:5173/boardinfo.html 접속하여 확인하기
 //  http://localhost:5173/ 접속하여 확인하기
 
+import BoardInfo from "@/pages/board/BoardInfo";
+
 function App() {
   return (
-    <h1>vite-template</h1>
+    <BoardInfo />
   );  
 }
 
